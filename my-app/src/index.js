f9fecd3c71992a1b1b8264b106da0b2788f2f1d2
anjayluh <anjayluhgags.wakabi@gmail.com>
2017-04-24 18:57:54 +0300

    Interactive component

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -2,10 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 class Square extends React.Component {
+  constructor(){
+    super();
+    this.state = {
+      value : null,
+    };
+  }
   render() {
     return (
-      <button className="square" onClick={() => alert('click')}>
-        {/* TODO */}
+      <button className="square" onClick={() => this.setState({value: 'X'})}>
+        {this.state.value}
       </button>
     );
   }
