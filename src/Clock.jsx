220a4f481f903183862b6577dc072660470db078
anjayluh <anjayluhgags.wakabi@gmail.com>
2017-04-30 01:44:14 +0300

    Lifecycle

--- src/Clock.jsx
+++ src/Clock.jsx
@@ -5,10 +5,21 @@ export default class Clock extends React.Component {
     super(props);
     this.state = { date: new Date() };
   }
+  componentDidMount() {
+    setInterval(() => this.updateCurrentTime(), 1000);
+  }
+  componentWillUnmount() {
+    clearInterval(this.timerID);
+  }
+  updateCurrentTime() {
+    this.setState({
+      currentTime: (new Date()).toString()
+    });
+  }
+
   render() {
     return (
       <div>
-        <h1>Hello, world!</h1>
         <h2>It is {this.state.date.toLocaleTimeString()}.</h2>
       </div>
     );
